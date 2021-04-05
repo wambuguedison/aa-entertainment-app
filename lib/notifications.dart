5788ef27e3db3d8ad8eac0c3c5e3f7b9eefb37ab
Eddie Waweru <wambuguedison@gmail.com>
2021-04-05 17:56:26 +0300

    loop for children

--- lib/notifications.dart
+++ lib/notifications.dart
@@ -13,29 +13,37 @@ class _NotificationsPageState extends State<NotificationsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Notifications"),
-        centerTitle: true,
-      ),
-      body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Card(
-              child: InkWell(
-            splashColor: Colors.blue.withAlpha(30),
-            onTap: () {
-              print('Card tapped.');
-            },
-            child: const SizedBox(
-              width: double.infinity,
-              height: 50,
-              child: Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text('This is a notification card'),
-              ),
-            ),
-          ))),
-      // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        appBar: AppBar(
+          title: Text("Notifications",
+              style: TextStyle(
+                fontFamily: "IndieFlower",
+              )),
+          centerTitle: true,
+        ),
+        body: Container(
+            child: Column(
+                children: new List.generate(
+                    10,
+                    (index) => Padding(
+                        padding: const EdgeInsets.only(
+                            left: 8.0, right: 8.0, top: 8.0),
+                        child: Card(
+                            child: InkWell(
+                          splashColor: Colors.blue.withAlpha(30),
+                          onTap: () {
+                            print('Card tapped.');
+                          },
+                          child: const SizedBox(
+                            width: double.infinity,
+                            height: 50,
+                            child: Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Text('This is a notification card'),
+                            ),
+                          ),
+                        ))))))
+        // This trailing comma makes auto-formatting nicer for build methods.
+        );
   }
   // This trailing comma makes auto-formatting nicer for build methods.
 }
